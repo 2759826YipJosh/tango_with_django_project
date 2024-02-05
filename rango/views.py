dfa33ef91c456b0881a3c6bb6190c49c3ba71b8b
Josh Yip <2759826Y@student.gla.ac.uk>
2024-02-05 14:50:29 +0000

    chapter 8 fini

--- rango/views.py
+++ rango/views.py
@@ -26,9 +26,21 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 
+# def about(request):
+#     context_dict = {'aboutmessage': 'This tutorial has been put together by Josh Yip'}
+#     return render(request, 'rango/about.html', context=context_dict)
+
+
 def about(request):
-    context_dict = {'aboutmessage': 'This tutorial has been put together by Josh Yip'}
-    return render(request, 'rango/about.html', context=context_dict)
+# prints out whether the method is a GET or a POST
+    print(request.method)
+# prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
+
+
+
+
 
 
 def show_category(request, category_name_slug):
