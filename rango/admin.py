e4005d5cdc36e14c872f9a7fc1ee0ea556e3d873
Josh Yip <2759826Y@student.gla.ac.uk>
2024-02-05 15:30:02 +0000

    upto chapter 9.7

--- rango/admin.py
+++ rango/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from rango.models import UserProfile
 
 # Register your models here.
 
@@ -21,4 +22,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Page, PageAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(UserProfile)
+
 
